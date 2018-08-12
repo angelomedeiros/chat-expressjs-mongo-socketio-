c63f1998990eb9424c5f3a9e98734d686d64a957
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-12 04:08:57 -0300

    Hello world usando o express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import * as express from 'express'
+
+var app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...')
+})
