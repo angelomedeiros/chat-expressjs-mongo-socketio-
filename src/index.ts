cd2ae40f4f40b4464f5f809cb9565da8d75b1233
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-12 04:34:44 -0300

    Hello world from post

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
+app.post('/', (req, res) => {
+  res.send('Hello world from method post!')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...')
 })
