4560f840fa4567a8ceda01acfb3e489882199aa5
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-12 04:53:33 -0300

    usando o next()

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ app.post('/', (req, res) => {
   res.send('Hello world from method post!')
 })
 
+app.get('/next', (req, res, next) => {
+  console.log('Test of next()')
+  next()
+}, (req, res) => {
+  res.send('Hello after next()')
+})
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...')
 })
