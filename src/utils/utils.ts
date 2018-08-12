63cfb614634f969dc8cd6b90ea03f5315c531582
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-12 15:33:52 -0300

    Add o debug e o morgan

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,45 +1,57 @@
-import { Server } from "http";
+import app from '../app'
+import * as debug from 'debug'
+import * as http from 'http'
 
-export const normalizePort = (val: number | string): number => {
-    return (typeof val === 'string') ? parseInt(val) : val
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
+
+const server = http.createServer(app)
+
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
 }
 
-export const onError = (server: Server) => {
-    return (error: NodeJS.ErrnoException): void => {
-        let port: number | string = server.address().port;
-        if (error.syscall !== 'listen') throw error;
-        let bind = (typeof port === 'string') ? `pipe ${port}` : `port ${port}`;
-        switch(error.code) {
-            case 'EACCES':
-                console.error(`${bind} requires elevated privileges`);
-                process.exit(1);
-                break;
-            case 'EADDRINUSE':
-                console.error(`${bind} is already in use`);
-                process.exit(1);
-                break;
-            default:
-                throw error;
-        }
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
     }
 }
 
-export const onListening = (server: Server) => {
-    return (): void => {
-        let addr = server.address();
-        let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
-        console.log(`Listening at ${bind}...`);
-    }
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port
+    debug('Listening on ' + bind)
 }
-
-export const handleError = (error: Error) => {
-    let errorMessage: string = `${error.name}: ${error.message}`
-    if ( process.env.NODE_ENV !== 'test' ) { console.log(errorMessage) }
-    return Promise.reject(new Error(errorMessage))
-}
-
-export const throwError = (condition: boolean, message: string): void => {
-    if (condition) { throw new Error(message) }
-}
-
-export const JWT_SECRET: string = process.env.JWT_SECRET
