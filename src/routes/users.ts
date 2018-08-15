f45e43744da5b5ffc6a93c2e5237d5ec21863aac
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-15 05:08:37 -0300

    Refatoramento da clase UserRoute

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,119 +1,29 @@
 import * as express from 'express'
 import * as mongoose from 'mongoose'
+import { CrudModel } from '../services/crudModel'
 import { User } from '../models/users.model'
+import { IUser } from '../interfaces/IUser'
 
-class UsersRouter {
+class UsersRouter extends CrudModel<IUser> {
 
   public mongooseConnection: mongoose.Connection
   public mongooseInstance  : any
   public router            : express.Router
 
   constructor() {
+    super(User)
     this.router = express.Router()
     this.Rotas()
     this.dbConnection()
   }
 
   Rotas () {
-
-    // FindAll
-    this.router.get('/', (req, res) => {
-      User.find()
-          .then( users => {
-            console.log('Succeess', users)
-            res.send(users)
-          })
-          .catch( e => {
-            console.log('Succeess', e)
-            res.send(e)
-          })
-      // console.log(req.params.id)
-    })
-
-    // FindById
-
-    this.router.get('/:id', (req, res) => {
-      // User.findById(req.params.id)
-      //     .then( user => {
-      //       console.log('findById ', user)
-      //       res.send(user)
-      //     })
-      //     .catch( err => {
-      //       console.log('findById error ', err)
-      //     })
-      User.findById(req.params.id, '-_id -__v', (err, user) => {
-        if (err) {
-          console.log('findById error ', err)
-          return res.send(err)
-        }
-        console.log('findById ', user)
-        res.send(user)
-      })
-    })
-
-    // Create new User
-    this.router.post('/', (req, res) => {
-      const teste = new User(req.body)
-      teste.save()
-           .then( x => {
-             res.send(x)
-            }
-          )
-          .catch( e => {
-              res.send(e)
-            }
-          )
-      // console.log('Teste')
-      // const { nome } = req.body
-      // console.log(nome)
-      // res.send('Olá angelo')
-    })
-
-    // this.router.post('/:id', (req, res) => {
-    //   User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
-    //     console.log('HAHAHAHAHAHAHAHAHAHAHAH')
-    //     if (err) {
-    //       console.log('findByIdAndUpdate error ', err)
-    //       user.save()
-    //       return res.send(err)
-    //     }
-    //     console.log('findBfindByIdAndUpdateyId ', user)
-    //     res.send(user)
-    //   })
-    // })
-
-    // Update user
-    this.router.post('/:id', (req, res) => { // Retorna o objeto encontrado
-      const query = User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-
-      query.select('-_id -__v')
-
-      query.exec( (err, user) => {
-        if (err) return res.send(err)
-        res.send(user)
-      })
-    })
-
-    // Delete User
-    this.router.delete('/:id', (req, res) => {
-      User.findByIdAndRemove(req.params.id).exec( (err, user) => {
-        console.log('Removeu... ', user)
-        res.send({ status: true })
-      })
-    })
-
-    // Usando o findOne
-    this.router.get('/findOne', (req, res) => {  //Não é canse sensitive o path
-      const query = User.findOne(req.query)
-      query.select('-_id cpf') // Traz o CPF e omite o _id
-
-      query.exec( (err, user) => {
-        if (err) return console.log('Errouu...', err)
-        res.send(user)
-      })
-
-    })
-
+    this.router.get('/',  this.findAll)
+    this.router.get('/:id', this.findById)
+    this.router.post('/', this.create)
+    this.router.post('/:id', this.update)
+    this.router.delete('/:id',this.delete)
+    this.router.get('/findOne', this.findOne)
   }
 
   dbConnection () {
---
+++ src/services/crudModel.ts
@@ -0,0 +1,68 @@
+import * as mongoose from 'mongoose'
+import { Response } from 'express';
+
+export abstract class CrudModel<T extends mongoose.Document> {
+
+  constructor (protected model: mongoose.Model<T>) {
+    // this.model = model
+  }
+
+  protected findAll = (req, res) => {
+    this.model
+          .find()
+          .then( result => {
+            res.send(result)
+          })
+          .catch( e => {
+            res.send(e)
+          })
+  }
+
+  protected findById = (req, res) => {
+    this.model.findById(req.params.id, '-_id -__v', (err, result) => {
+      if (err) {
+        return res.send(err)
+      }
+      res.send(result)
+    })
+  }
+
+  protected create = (req, res: Response) => {
+    const document = new this.model(req.body)
+    document.save()
+            .then( x => {
+              res.send(x)
+            })
+            .catch( e => {
+              res.status(401)
+              res.send(e)
+            })
+  }
+
+  protected update = (req, res) => {
+    const query = this.model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    query.select('-_id -__v')
+    query.exec((err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+  }
+
+  protected delete = (req, res) => {
+    this.model.findByIdAndRemove(req.params.id)
+      .exec( (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  }
+
+  protected findOne = (req, res) => {
+    const query = this.model.findOne(req.query)
+      query.select('-_id cpf')
+      query.exec( (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  }
+
+}
