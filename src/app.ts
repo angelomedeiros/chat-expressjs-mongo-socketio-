a47a9dd3420f3e3a198e6bebd102787a8444707c
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-08-16 02:23:11 -0300

    Add o swagger e configuracao

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import * as express from 'express'
+import * as path from 'path'
 import * as bodyParser from 'body-parser'
 import UsersRouter from './routes/users'
 import IndexRouter from './routes'
 import DataAccess from './config/dataAccess'
+import * as swaggerUi from 'swagger-ui-express'
+import * as YAML from 'yamljs'
+
+const swaggerDocument = YAML.load(path.join(__dirname, '/docs/swagger.yaml'))
 
 class App {
 
@@ -11,13 +16,18 @@ class App {
 
   constructor() {
     this.app = express()
+    this.middlewares()
     this.routes()
     this.db()
   }
 
-  private routes() {
+  private middlewares () {
     this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded({ extended: false })) // Pesquisar o que isso faz!!
+    this.app.use(bodyParser.urlencoded({ extended: false })) // Pesquisar o que isso faz!!    
+    this.app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  }
+
+  private routes() {
     this.app.use('/', IndexRouter)
     this.app.use('/users', UsersRouter)    
   }
